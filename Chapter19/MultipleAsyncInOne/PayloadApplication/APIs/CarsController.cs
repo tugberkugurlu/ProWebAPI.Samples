66968ea7b3d946b7d722e2f3dbfee0a5ee770893
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-05 16:06:29 +0300

    completed Ch19-MultipleAsyncInOne sample

---
+++ Chapter19/MultipleAsyncInOne/PayloadApplication/APIs/CarsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Web.Http;
+using PayloadApplication.Models;
+
+namespace PayloadApplication.APIs {
+
+    public class CarsController : ApiController {
+
+        readonly CarsContext _carsContext = new CarsContext();
+
+        public IEnumerable<Car> Cheap() {
+
+            Thread.Sleep(1000);
+
+            return _carsContext.GetCars(car => car.Price < 50000);
+        }
+
+        public IEnumerable<Car> Expensive() {
+
+            Thread.Sleep(1000);
+
+            return _carsContext.GetCars(car => car.Price >= 50000);
+        }
+    }
+}

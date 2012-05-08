156c81f732b6cb22a904078d023b5c16eb4a3e2e
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-08 14:20:04 +0300

    completed Asynchronous HTTP Requests section

---
+++ Chapter19/HttpRequests/HttpRequests/APIs/AsyncCarsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using HttpRequests.Models;
+
+namespace HttpRequests.APIs {
+
+    public class AsyncCarsController : ApiController {
+
+        //HTTP service base address
+        const string _countryAPIBaseAddress = "http://localhost:11338/api/cars";
+
+        public async Task<IEnumerable<Car>> Get() {
+
+            using (HttpClient httpClient = new HttpClient()) {
+
+                var response = await httpClient.GetAsync(_countryAPIBaseAddress);
+                var content = await response.Content.ReadAsAsync<List<Car>>();
+
+                return content.Where(x => x.Price > 30000.00F);
+            }
+        }
+    }
+}
---
+++ Chapter19/HttpRequests/HttpRequests/APIs/SyncCarsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using HttpRequests.Models;
+
+namespace HttpRequests.APIs {
+
+    public class SyncCarsController : ApiController {
+
+        const string _countryAPIBaseAddress = "http://localhost:11338/api/cars";
+
+        public IEnumerable<Car> Get() {
+
+            using (HttpClient httpClient = new HttpClient()) {
+
+                var response = httpClient.GetAsync(_countryAPIBaseAddress).Result;
+                var content = response.Content.ReadAsAsync<List<Car>>().Result;
+
+                return content;
+            }
+        }
+    }
+}

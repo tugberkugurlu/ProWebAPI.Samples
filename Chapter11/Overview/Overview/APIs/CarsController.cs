d16027ec4bfade88d726de91e25a961bfbf02efb
tugberkugurlu <tugberkugurlu@gmail.com>
2012-05-23 00:48:05 +0300

    initial ch11 commit.

---
+++ Chapter11/Overview/Overview/APIs/CarsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Overview.Models;
+
+namespace Overview.APIs {
+
+    public class CarsController : ApiController {
+
+        private readonly CarsContext carsContext = new CarsContext();
+
+        public IEnumerable<Car> GetCars() {
+
+            return carsContext.All;
+        }
+    }
+}

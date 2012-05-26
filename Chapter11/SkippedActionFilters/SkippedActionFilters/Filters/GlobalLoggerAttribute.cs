3aa69702c052bbb3bab1b1fa13199a4a8fb05d32
tugberkugurlu <tugberkugurlu@gmail.com>
2012-05-26 19:10:26 +0300

    added SkippedActionFilters and it is approved. I also added ExceptionFilterOrdering proj which I didn't use inside the book but explained a little

---
+++ Chapter11/SkippedActionFilters/SkippedActionFilters/Filters/GlobalLoggerAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+
+namespace SkippedActionFilters.Filters {
+
+    public class GlobalLoggerAttribute : ActionFilterAttribute {
+
+        private const string _loggerName = "GlobalLogger";
+
+        public override void OnActionExecuting(HttpActionContext actionContext) {
+
+            LoggerUtils.WriteLog(
+                _loggerName,
+                "OnActionExecuting",
+                actionContext.ControllerContext.ControllerDescriptor.ControllerName,
+                actionContext.ActionDescriptor.ActionName
+            );
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext) {
+
+            LoggerUtils.WriteLog(
+                _loggerName,
+                "OnActionExecuted",
+                actionExecutedContext.ActionContext.ControllerContext.ControllerDescriptor.ControllerName,
+                actionExecutedContext.ActionContext.ActionDescriptor.ActionName
+            );
+        }
+    }
+}
---
+++ Chapter11/SkippedActionFilters/SkippedActionFilters/Filters/SecondaryLoggerAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Diagnostics;
+using System.Web.Http.Controllers;
+
+namespace SkippedActionFilters.Filters {
+
+    public class SecondaryLoggerAttribute : ActionFilterAttribute {
+
+        private const string _loggerName = "SecondaryLogger";
+
+        public override void OnActionExecuting(HttpActionContext actionContext) {
+
+            LoggerUtils.WriteLog(
+                _loggerName,
+                "OnActionExecuting",
+                actionContext.ControllerContext.ControllerDescriptor.ControllerName,
+                actionContext.ActionDescriptor.ActionName
+            );
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext) {
+
+            LoggerUtils.WriteLog(
+                _loggerName,
+                "OnActionExecuted",
+                actionExecutedContext.ActionContext.ControllerContext.ControllerDescriptor.ControllerName,
+                actionExecutedContext.ActionContext.ActionDescriptor.ActionName
+            );
+        }
+    }
+}

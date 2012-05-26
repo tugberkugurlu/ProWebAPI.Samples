f529d3e858b455f72e731ba73867114f9a3cac9f
tugberkugurlu <tugberkugurlu@gmail.com>
2012-05-26 17:17:14 +0300

    started ActionFiltersAndOrdering proj

---
+++ Chapter11/ActionFiltersAndOrdering/ActionFiltersAndOrdering/Filters/LoggerAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Diagnostics;
+using System.Web.Http.Controllers;
+
+namespace ActionFiltersAndOrdering.Filters {
+
+    public class LoggerAttribute : ActionFilterAttribute {
+
+        private const string _loggerName = "Logger";
+
+        public override void OnActionExecuting(HttpActionContext actionContext) {
+
+            LoggerUtils.WriteLog(
+                _loggerName,
+                "OnActionExecuting",
+                actionContext.ControllerContext.ControllerDescriptor.ControllerName,
+                actionContext.ActionDescriptor.ActionName
+            );
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext) {
+
+            LoggerUtils.WriteLog(
+                _loggerName,
+                "OnActionExecuted",
+                actionExecutedContext.ActionContext.ControllerContext.ControllerDescriptor.ControllerName,
+                actionExecutedContext.ActionContext.ActionDescriptor.ActionName
+            );
+        }
+    }
+}
---
+++ Chapter11/ActionFiltersAndOrdering/ActionFiltersAndOrdering/Filters/LoggerUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+namespace ActionFiltersAndOrdering.Filters {
+
+    public class LoggerUtils {
+
+        public static void WriteLog(
+            string loggerName, string loggerMethodName, string controllerName, string actionName) {
+
+            Trace.TraceInformation(
+                string.Format(
+                    "{0}, {1} method for Controller {2}, Action {3} is running...",
+                    loggerName, loggerMethodName, actionName, controllerName
+                )
+            );
+        }
+    }
+}

17ac0cd65789bf0c1cbfa586eb0024332bd924c0
tugberkugurlu <tugberkugurlu@gmail.com>
2012-05-28 14:50:08 +0300

    clean up and adding new ExceptionFilters proj

--- Chapter11/ExceptionFilters/ExceptionFilters/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExceptionFilters.Global" Language="C#" %>
--- Chapter11/ExceptionFilters/ExceptionFilters/Properties/AssemblyInfo.cs
+++ Chapter11/ExceptionFilters/ExceptionFilters/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4d02f8fa-c9ea-45ee-977d-70061c6c8ded")]
+[assembly: Guid("5886b8d5-7964-4637-a579-8ee414322357")]
 
 // Version information for an assembly consists of the following four values:
 //

42cd6cec90c3cec43fb38493ed2ad7cde0ca221f
tugberkugurlu <tugberkugurlu@gmail.com>
2012-05-28 15:44:04 +0300

    added ExceptionFilters proj and approved it

---
+++ Chapter11/ExceptionFilters/ExceptionFilters/Filters/HandleErrorAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Diagnostics;
+
+namespace ExceptionFilters.Filters {
+
+    public class HandleErrorAttribute : ExceptionFilterAttribute {
+
+        public override void OnException(HttpActionExecutedContext actionExecutedContext) {
+
+            Trace.TraceInformation(
+                "Exception occured. Controller: {0}, action: {1}. Exception message: {2}",
+                actionExecutedContext.ActionContext
+                    .ControllerContext.ControllerDescriptor.ControllerName,
+                actionExecutedContext.ActionContext.ActionDescriptor.ActionName,
+                actionExecutedContext.Exception.Message
+            );
+        }
+    }
+}

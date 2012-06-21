297f62d7b87e53acbb479fad9a88e49f182b788f
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-06-22 00:26:52 +0300

    added the AutofacWebAPISample inside the TryOuts

---
+++ @TryOuts/AutofacWebAPISample/AutofacWebAPISample/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AutofacWebAPISample {
+
+    public class WebApiConfig {
+
+        public static void Configure(HttpConfiguration config) {
+
+            RegisterRoutes(config);
+            AutofacWebAPI.Initialize(config);
+        }
+
+        public static void RegisterRoutes(HttpConfiguration config) {
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+    }
+}

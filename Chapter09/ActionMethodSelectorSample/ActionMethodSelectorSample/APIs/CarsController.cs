7fe3bcfb4caddf7bc447da5a777e4ddc4f93d36b
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-14 18:11:54 +0300

    completed FirstActionSample and ActionHttpMethodProviderSample projes

---
+++ Chapter09/ActionMethodSelectorSample/ActionMethodSelectorSample/APIs/CarsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ActionMethodSelectorSample.APIs {
+
+    public class CarsController : ApiController {
+
+        public string[] Cars() {
+
+            return cars();
+        }
+
+        private string[] cars() {
+
+            return new string[] { 
+                "Car 1",
+                "Car 2",
+                "Car 3"
+            };
+        }
+    }
+}

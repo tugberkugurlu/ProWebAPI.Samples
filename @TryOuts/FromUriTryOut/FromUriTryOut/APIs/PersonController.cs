4c8d348f193ce7e959c1119d47f29f370f93a82c
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-19 11:21:01 +0300

    added FromUriTryOut in @TryOuts folder

---
+++ @TryOuts/FromUriTryOut/FromUriTryOut/APIs/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FromUriTryOut.APIs {
+
+    public class Person
+	{
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+	}
+
+    public class PersonController : ApiController {
+
+        //POST /api/person?Name=Foo&Surname=Bar&Age=17
+        public Person Post([FromUri]Person person) {
+
+            return person;
+        }
+    }
+}

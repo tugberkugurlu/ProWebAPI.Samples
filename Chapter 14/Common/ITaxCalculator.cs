85633b95c6284d4c0d897d7a65239eab32208746
Ali Kheyrollahi <aliostad@gmail.com>
2013-04-07 22:57:45 +0100

    complete overhaul of the structure

--- Chapter 14/Common/ITaxCalculator.cs
+++ Chapter 14/Common/ITaxCalculator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AspNetWebApi.DependencyResolution.AutoFac
+namespace AspNetWebApi.DependencyResolution
 {
     public interface ITaxCalculator
     {
--- Chapter 14/Common/ValuesController.cs
+++ Chapter 14/Common/ValuesController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace AspNetWebApi.DependencyResolution.AutoFac
+namespace AspNetWebApi.DependencyResolution
 {
     public class ValuesController : ApiController
     {

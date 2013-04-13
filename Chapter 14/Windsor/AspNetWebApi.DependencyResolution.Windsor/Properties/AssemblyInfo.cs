d0b7ea53d2afafc41e72fc21452fc0e0cddfe0c1
Ali Kheyrollahi <aliostad@gmail.com>
2013-04-13 16:55:20 +0100

    adding windsor

--- Chapter 14/Windsor/AspNetWebApi.DependencyResolution.Windsor/Properties/AssemblyInfo.cs
+++ Chapter 14/Windsor/AspNetWebApi.DependencyResolution.Windsor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AspNetWebApi.DependencyResolution.AutoFac.Tests")]
+[assembly: AssemblyTitle("AspNetWebApi.DependencyResolution.Windsor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AspNetWebApi.DependencyResolution.AutoFac.Tests")]
+[assembly: AssemblyProduct("AspNetWebApi.DependencyResolution.Windsor")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7409705-feb2-4d14-b765-e9688fb4cf1b")]
+[assembly: Guid("7e084394-f64a-494b-bb45-2d69c14e3957")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
